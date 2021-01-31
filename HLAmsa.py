d64bfeb9d3398020681740c1666fc575da1119dc
linnil1 <linnil1.886@gmail.com>
2021-01-31 19:45:26 +0800

    Add feature of chunk selection and specific output format

--- HLAmsa.py
+++ HLAmsa.py
@@ -104,6 +104,82 @@ class Genemsa:
                            if re.match(regex, allele)}
         return new_msa
 
+    def select_exon(self, exon_index=[]):
+        """
+        Extract exon from gene
+
+        exon_index: list
+            The index of exon you want to extract
+            Index start from 1
+            e.g.
+              1 for exon1
+              2 for exon2
+            Leave empty if you want all the exons
+        """
+        assert self.seq_type == "gen"
+        assert len(self.blocks) % 2 == 1
+        assert not len(exon_index) or (
+                max(exon_index) <= len(self.blocks) // 2 and
+                min(exon_index) > 0)
+
+        # If not specific the index, extract all exons
+        if not exon_index:
+            exon_index = range(1, len(self.blocks), 2)
+        else:
+            exon_index = [i * 2 - 1 for i in exon_index]
+
+        new_msa = self.select_region(exon_index)
+        new_msa.seq_type = "nuc"
+        for allele, seq in new_msa.alleles.items():
+            assert "E" not in seq
+        return new_msa
+
+    def select_region(self, index=[]):
+        """
+        Extract intron and exon from gene
+
+        index: list
+            The index of exon you want to extract
+            Index start from 1
+            Leave empty if you want all the exons
+            e.g.
+              0 for 5-UTR
+              1 for exon1
+              2 for intron1
+              3 for exon2
+              4 for 3-UTR(for two exons gene)
+              -1 for 3-UTR(for all case)
+        """
+        assert not len(index) or (
+                max(index) <= len(self.blocks) and
+                min(index) >= -1)
+
+        # replace -1 (3-UTR)
+        for i in range(len(index)):
+            if index[i] == -1:
+                index[i] = len(self.blocks) - 1
+
+        # new a msa object
+        new_msa = Genemsa(self.gene_name, "")
+        for i in index:
+            new_msa.blocks.append(self.blocks[i])
+
+        # extract
+        gen_pos = self.calculate_position()
+        for allele, gen_seq in self.alleles.items():
+            new_seq = ""
+            for i in index:
+                new_seq += gen_seq[gen_pos[i]:gen_pos[i + 1]]
+            new_msa.alleles[allele] = new_seq
+
+        return new_msa
+
+    def calculate_position(self):
+        pos = [0]
+        for length in self.blocks:
+            pos.append(pos[-1] + length)
+        return pos
+
     def merge_exon(self, msa_nuc):
         """ Merge nuc into gen """
         # merge when allele in both nuc and gen
@@ -111,13 +187,8 @@ class Genemsa:
         assert self.gene_name == msa_nuc.gene_name
         assert len(self.blocks) == len(msa_nuc.blocks) * 2 + 1
 
-        # init
-        nuc_pos = [0]
-        for length in msa_nuc.blocks:
-            nuc_pos.append(nuc_pos[-1] + length)
-        gen_pos = [0]
-        for length in self.blocks:
-            gen_pos.append(gen_pos[-1] + length)
+        nuc_pos = msa_nuc.calculate_position()
+        gen_pos = self.calculate_position()
         new_msa = Genemsa(self.gene_name, self.seq_type)
         ref_blocks = []
 
@@ -138,7 +209,7 @@ class Genemsa:
 
                     # exon
                     nuc_seq_part = nuc_seq[nuc_pos[i // 2]:nuc_pos[i // 2 + 1]]
-                    if nuc_seq_part.replace(".", "") != gen_seq_part.replace(".", ""):
+                    if nuc_seq_part.replace("-", "") != gen_seq_part.replace("-", ""):
                         # Special Case: ignore exon5 in DQB1
                         if not (i == 9 and allele.startswith("DQB1")):
                             # print(nuc_seq_part)
@@ -209,6 +280,7 @@ class Genemsa:
         for allele in alleles:
             alleles[allele] = alleles[allele].replace(" ", "").replace("*", ".")
             if allele == ref_allele:
+                alleles[allele] = alleles[allele].replace(".", "-")
                 continue
             ref_seq = alleles[ref_allele]
             if len(alleles[allele]) != len(ref_seq):
@@ -225,14 +297,89 @@ class Genemsa:
                     assert seq[i] == ref_seq[i]
                 else:
                     assert seq[i] in "ATCG."
-            alleles[allele] = ''.join(seq)
+            alleles[allele] = ''.join(seq).replace(".", "-")
 
         for allele in rm_allele:
             logging.warning(f"Remove {allele} due to length in {fname}")
             del alleles[allele]
         return alleles
 
+    def format_alignment_diff(self, ref_allele=""):
+        """ Print all alleles diff from ref_allele """
+        assert self.alleles
+        if not ref_allele:
+            ref_allele = next(iter(self.alleles.keys()))
+        assert ref_allele in self.alleles
+        ref_seq = self.alleles[ref_allele]
+
+        # use new msa object to save sequences
+        new_msa = Genemsa(self.gene_name, "")
+        new_msa.blocks = self.blocks
+        new_msa.alleles = {ref_allele: ref_seq}
+        for allele, seq in self.alleles.items():
+            if allele == ref_allele:
+                continue
+            new_seq = ""
+            for i in range(len(seq)):
+                if seq[i] == ref_seq[i]:
+                    new_seq += "-"
+                elif seq[i] == "-":
+                    new_seq += "*"
+                elif seq[i] == ".":
+                    new_seq += "*"
+                else:
+                    new_seq += seq[i]
+            new_msa.alleles[allele] = new_seq
+        return new_msa.format_alignment()
+
+    def format_alignment(self, wrap=100):
+        assert self.blocks and self.alleles
+        output_str = ""
+        bid = 0
+        block_pos = self.calculate_position()
+        block_pos, seq_length = block_pos[1:-1], block_pos[-1]
+
+        for pos in range(0, seq_length, wrap):
+            # Find the insertion point
+            seq_part_len = min(seq_length, pos + wrap) - pos
+            split_data = [(seq_part_len, 3, "\n")]  # pos, priority, char
+            split_data += [(i, 2, " ") for i in range(10, seq_part_len, 10)]
+            while bid < len(block_pos) and pos < block_pos[bid] <= pos + seq_part_len:
+                split_data.append((block_pos[bid] - pos, 1, "|"))
+                bid += 1
+            split_data = sorted(split_data)
+
+            # Header per wrapping
+            output_str += f" {'gDNA':<18} {pos}\n"
+            output_str += " " * 20 + "|\n"
+
+            # alleles
+            for allele, seq in self.alleles.items():
+                output_str += f" {allele:18} "
+                cur_pos = pos
+                for j in split_data:
+                    if j[0] > len(seq):
+                        break
+                    output_str += seq[cur_pos:pos + j[0]]
+                    output_str += j[2]
+                    cur_pos = pos + j[0]
+            output_str += "\n\n"
+        return output_str
+
+    def to_biopython(self):
+        from Bio.Align import MultipleSeqAlignment
+        from Bio.SeqRecord import SeqRecord
+        from Bio.Seq import Seq
+
+        bio_msa = MultipleSeqAlignment([
+            SeqRecord(Seq(seq), id=allele, description="")
+            for allele, seq in self.alleles.items()])
+        return bio_msa
+
 
 if __name__ == "__main__":
-    # msa = HLAmsa(["A"], imgt_folder="alignments")
-    msa = HLAmsa([], imgt_folder="alignments")
+    hla = HLAmsa(["A"], imgt_folder="alignments")
+    a_select = hla.genes["A"].select_allele(r"A\*.*:01:01:01$")
+    print(a_select.to_biopython().format("fasta"))
+    # print(a_select.select_exon().format_alignment())
+    # print(a_select.format_alignment_diff())
