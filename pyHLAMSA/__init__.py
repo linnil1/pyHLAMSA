c6ace90cf81cf63228595a38c5667b907cc20d88
linnil1 <linnil1.886@gmail.com>
2021-02-13 16:17:16 +0800

    Add function of reading MSF

--- pyHLAMSA/__init__.py
+++ pyHLAMSA/__init__.py
@@ -7,6 +7,7 @@ from pprint import pprint
 from typing import List, Tuple, Dict
 
 from Bio.Align import MultipleSeqAlignment, PairwiseAligner
+from Bio import AlignIO
 from Bio.SeqRecord import SeqRecord
 from Bio.Seq import Seq
 import pysam
@@ -925,6 +926,178 @@ class Genemsa:
             for record in records:
                 f.write("\t".join(record) + "\n")
         return records
+
+    def read_MSF_file(self, file_msf):
+        """ Read .msf file """
+        new_msa = Genemsa.from_MultipleSeqAlignment(AlignIO.read(file_msf, "msf"))
+        self.alleles = new_msa.alleles
+        self.blocks = new_msa.blocks
+        self.labels = new_msa.labels
+        return self
+
+    @classmethod
+    def read_dat(cls, file_dat):
+        """
+        Read .dat file
+
+        Args:
+          file_dat(str): File path to xx.dat
+
+        Returns:
+          dat_object(dict): {allele_name: [[block_name(str), start(int), end(int)],]
+              ,where block_name is intron1, exon1, ...
+        """
+        now_allele = ""
+        read_next = False
+        data = {}
+
+        for line in open(file_dat):
+            # read allele name
+            if "allele=" in line:
+                now_allele = line.split('"')[1]
+                # no duplicated allele_name
+                assert now_allele not in data
+                data[now_allele] = []
+
+            # read blocks
+            elif re.match(r"FT\s+((UTR)|(exon)|(intron))", line):
+                line = line.split()
+                data[now_allele].append([line[1], *map(lambda a: int(a), line[2].split(".."))])
+                if line[1] != "UTR":
+                    read_next = True
+
+            # read block name .e.g. exon1 exon2
+            elif read_next:
+                read_next = False
+                data[now_allele][-1][0] += line.split('"')[1]
+
+        # make sure the order start and end position in dat are correct
+        for allele in data:
+            data[allele] = list(sorted(data[allele], key=lambda a: a[1]))
+
+        return data
+
+    def merge_dat(self, dat) -> Genemsa:
+        """
+        Merge dat object
+
+        Args:
+          dat(object): Objected create from `Genemsa.read_dat`
+
+        Returns:
+            Genemsa
+        """
+        if not len(self.alleles):
+            raise ValueError("MSA is empty")
+        msf_length = len(self._get_first()[1])
+        if self.seq_type != "gen" and self.seq_type != "nuc":
+            raise TypeError("Check this object is gen or nuc")
+        dat = copy.deepcopy(dat)
+
+        # Check consistent between dat and msf
+        alleles = self.alleles
+        block_cord = {}
+        new_alleles = {}
+        for allele, seq in alleles.items():
+            # Check allele name in the dat
+            # and sequence is consistent to dat
+            hla_name = "HLA-" + allele
+            assert hla_name in dat
+
+            # extract exon region
+            if self.seq_type == "nuc":
+                dat_hla = []
+                end = 0
+                for i in [i for i in dat[hla_name] if "exon" in i[0]]:
+                    gap = i[1] - end - 1
+                    dat_hla.append([i[0], i[1] - gap, i[2] - gap])
+                    end = dat_hla[-1][2]
+                dat[hla_name] = dat_hla
+
+            print(allele, dat[hla_name])
+            if len(seq.replace("-", "")) != dat[hla_name][-1][2]:
+                logger.warning(f"Ignore {allele}, length is different from dat")
+                continue
+
+            # rename UTR
+            # assume blocks are ordered
+            if "UTR" == dat[hla_name][0][0]:
+                dat[hla_name][0][0] = "5UTR"
+            if "UTR" == dat[hla_name][-1][0]:
+                dat[hla_name][-1][0] = "3UTR"
+
+            # Coordination mapping from sequence to msf
+            seq_cord = []
+            assert len(seq) == msf_length
+            for i in range(len(seq)):
+                if seq[i] != "-":
+                    assert seq[i] in "ATCG"
+                    seq_cord.append(i)
+
+            # Get the position of each block in msf
+            old_cord = copy.deepcopy(block_cord)
+            for block_name, start, end in dat[hla_name]:
+                if block_name not in block_cord:
+                    block_cord[block_name] = [99999, 0]
+
+                block_cord[block_name][0] = min(block_cord[block_name][0],
+                                                seq_cord[start - 1])
+                block_cord[block_name][1] = max(block_cord[block_name][1],
+                                                seq_cord[end - 1] + 1)
+
+            # Check consistent
+            # Assume first allele has correct coordination
+            # , otherwise all allele will ignore
+            block_cord_list = list(sorted(block_cord.values()))
+            for i in range(1, len(block_cord_list)):
+                if block_cord_list[i][0] < block_cord_list[i - 1][1]:
+                    logger.warning(f"Ignore {allele}, msf inconsistent")
+                    block_cord = old_cord
+                    break
+            else:
+                new_alleles[allele] = seq
+
+        # Reset position of each block
+        # If perfect -> start_i == end_{i-1}
+        # If not -> start_i = end_{i-1}
+        block_cord_list = list(sorted(block_cord.items(), key=lambda i: i[1]))
+        # TODO: is end == msf_length
+        end = msf_length
+        # end = block_cord_list[-1][1][1]
+        for i in reversed(range(len(block_cord_list))):
+            block_cord_list[i], end = ([block_cord_list[i][0],
+                                        [block_cord_list[i][1][0], end]],
+                                       block_cord_list[i][1][0])
+
+        # Check the sequence length in msf is same as block coordination
+        alleles = new_alleles
+        new_alleles = {}
+        block_cord = dict(block_cord_list)
+        for allele, seq in alleles.items():
+            allele_block = {i[0]: i[2] - i[1] + 1 for i in dat["HLA-" + allele]}
+
+            for block_name in block_cord:
+                len_block = len(seq[block_cord[block_name][0]:block_cord[block_name][1]].replace("-", ""))
+                len_block_ref = allele_block.get(block_name, 0)
+                # should always true, otherwise ignore in above
+                assert len_block == len_block_ref
+            else:
+                new_alleles[allele] = seq
+
+        # To new object
+        new_msa = Genemsa(self.gene_name, self.seq_type)
+        new_msa.alleles = new_alleles
+        for name, (start, end) in block_cord_list:
+            new_msa.blocks.append(end - start)
+            if "3UTR" == name:
+                new_msa.labels.append(("three_prime_UTR", "3UTR"))
+            elif "5UTR" == name:
+                new_msa.labels.append(("five_prime_UTR", "5UTR"))
+            elif "exon" in name:
+                new_msa.labels.append(("exon", name))
+            elif "intron" in name:
+                new_msa.labels.append(("intron", name))
+        return new_msa
 
 
 setup_logger()
